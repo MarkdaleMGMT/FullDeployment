39ee41cf53192d247fe242b2d20dda82d0e93855
root@138.197.175.92 <root@138.197.175.92>
2019-08-27 19:54:37 +0000

    QOL fixes. Now showes current branch at the end

--- changeBranch.sh
+++ changeBranch.sh
@@ -6,6 +6,10 @@ branch=$2
 #Stash any changes in the repo and checkout the new branch
 cd $repo
 git stash --include-untracked #New files will be stashed alo
+git pull #Check for new files
 git checkout $branch
 git prune #Prune changed files
 git pull
+echo "\n"
+echo "Your Branch Is:"
+git branch
